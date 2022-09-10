1d141ae878140b6a090af3d5f4de2214920ebeb0
boyarkov_v <fluentzhiv@gmail.com>
2022-09-10 23:59:02 +0400

    дз готово

---
+++ 27/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+int Read (string message)
+{
+Console.WriteLine(message);
+return Convert.ToInt32(Console.ReadLine());
+}
+int sum (int a )
+{
+    int summ = 0;
+    if (a > 0 )
+    {
+        for (int i = a; i > 0; i= i / 10)
+        {
+            int ost =  i % 10;
+            summ = summ + ost;
+        }
+    return summ;
+    }
+else
+{
+Console.WriteLine("Ошибка! Введите положительное число!");
+return -1;
+}
+}
+int NumberA = Read("Введите положительное число");
+int summa = sum(NumberA);
+Console.WriteLine($"Сумма цифр в числе {NumberA} = {summa}");
---
+++ 29/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран. Данные вводятся с консоли пользователем
+int Read (string message)
+{
+Console.WriteLine(message);
+return Convert.ToInt32(Console.ReadLine());
+}
+
+int [] Numb  = new int[8];
+int i = 0 ;
+ while (i < Numb.Length)
+ {
+ int NumberA = Read("Введите любое число");
+ Numb[i] = NumberA;
+ i++;
+ }
+ Console.WriteLine("Создан новый массив: ");
+ i = 0;
+ while (i < Numb.Length)
+ {
+    Console.Write(Numb[i]+"   ");
+ i++;
+ }
